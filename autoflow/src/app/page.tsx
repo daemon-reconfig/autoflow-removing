8eb43c2150abc86ed48b82d23e9e5b4852115ba0
daemon-reconfig <mehulishi27@gmail.com>
2024-03-28 00:11:19 +0530

    Website

--- autoflow/src/app/page.tsx
+++ autoflow/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CardBody, CardContainer, CardItem } from "@/components/global/3d-card";
 import { HeroParallax } from "@/components/global/connect-parallex";
 import { ContainerScroll } from "@/components/global/container-scroll-anim";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
@@ -5,6 +6,7 @@ import { LampComponent } from "@/components/global/lamp";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
 import { clients, products } from "@/lib/constants";
+import { CheckIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -54,7 +56,147 @@ export default function Home() {
       <section className="mt-[-500px]">
         <LampComponent />
         <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
-
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]
+              dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto
+              rounded-xl p-6 border">
+                <CardItem 
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  Hobby
+                  <h2 className="text-6xl">0$</h2>
+                </CardItem>
+                <CardItem 
+                  translateZ="60"
+                  className="text-sm text-neutral-500 max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  A Glimpse of what AutoFlow can do for you
+                  <ul className="my-4 flex flex-col gap-2">
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />3 Free Automations
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />100 tasks per month
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />Two-step Actions
+                    </li>
+                  </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem 
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs dark:bg-white
+                  dark:text-black text-white font-bold bg-black"
+                  >
+                    Get Started
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]
+              dark:bg-black dark:border-white border-purple w-full md:!w-[350px] h-auto
+              rounded-xl p-6 border">
+                <CardItem 
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  Professional
+                  <h2 className="text-6xl">29$</h2>
+                </CardItem>
+                <CardItem 
+                  translateZ="60"
+                  className="text-sm text-neutral-500 max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  A Glimpse of what AutoFlow can do for you
+                  <ul className="my-4 flex flex-col gap-2">
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />3 Free Automations
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />100 tasks per month
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />Two-step Actions
+                    </li>
+                  </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem 
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs dark:bg-white
+                  dark:text-black text-white font-bold bg-black"
+                  >
+                    Get Started
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
+            <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1]
+              dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto
+              rounded-xl p-6 border">
+                <CardItem 
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  Unlimited
+                  <h2 className="text-6xl">99$</h2>
+                </CardItem>
+                <CardItem 
+                  translateZ="60"
+                  className="text-sm text-neutral-500 max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  A Glimpse of what AutoFlow can do for you
+                  <ul className="my-4 flex flex-col gap-2">
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />3 Free Automations
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />100 tasks per month
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <CheckIcon />Two-step Actions
+                    </li>
+                  </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem 
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl text-xs dark:bg-white
+                  dark:text-black text-white font-bold bg-black"
+                  >
+                    Get Started
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
         </div>
       </section>
     </main>
