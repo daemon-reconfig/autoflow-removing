a2b4c0fb46ab16674a148d12082af2cde214949b
daemon-reconfig <mehulishi27@gmail.com>
2024-03-28 10:49:08 +0530

    Infobar UI

--- autoflow/src/app/(main)/layout.tsx
+++ autoflow/src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import InfoBar from '@/components/infobar'
 import SideBar from '@/components/sidebar'
 import React from 'react'
 
@@ -8,6 +9,7 @@ const Layout = (props: Props) => {
     <div className='flex overflow-hidden h-screen'>
         <SideBar />
         <div className='w-full'>
+            <InfoBar />
             {props.children}
         </div>
     </div>
--- autoflow/src/components/sidebar/index.tsx
+++ autoflow/src/components/sidebar/index.tsx
@@ -11,6 +11,9 @@ import {
   } from "@/components/ui/tooltip"
 import { menuOptions } from '@/lib/constants'
 import clsx from 'clsx'
+import { Separator } from '../ui/separator'
+import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
+import { ModeToggle } from '../global/mode-toggle'
   
 
 type Props = {}
@@ -56,6 +59,52 @@ const SideBar = (props: Props) => {
                     </ul>
                 ))}
             </TooltipProvider>
+            <Separator />
+            <div className='flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 
+            rounded-full h-56 overflow-scroll border-[1px]'>
+                <div className='relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px]
+                border-[1px] dark:border-t-[#353346]'>
+                    <LucideMousePointerClick 
+                        className='dark:text-white'
+                        size={18}
+                    />
+                    <div className='border-l-2 border-muted-foreground/50 h-6 absolute 
+                    left-1/2 transform translate-x-[-50%] -bottom-[30px]' />
+                    
+                </div>
+                <div className='relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px]
+                border-[1px] dark:border-t-[#353346]'>
+                    <GitBranch 
+                        className='dark:text-white'
+                        size={18}
+                    />
+                    <div className='border-l-2 border-muted-foreground/50 h-6 absolute 
+                    left-1/2 transform translate-x-[-50%] -bottom-[30px]' ></div>
+                    
+                </div>
+                <div className='relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px]
+                border-[1px] dark:border-t-[#353346]'>
+                    <Database
+                        className='dark:text-white'
+                        size={18}
+                    />
+                    <div className='border-l-2 border-muted-foreground/50 h-6 absolute 
+                    left-1/2 transform translate-x-[-100%] -bottom-[30px]' />
+                    
+                </div>
+                <div className='relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px]
+                border-[1px] dark:border-t-[#353346]'>
+                    <LucideMousePointerClick 
+                        className='dark:text-white'
+                        size={18}
+                    />
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div className='flex items-center justify-center flex-col gap-8'>
+            <ModeToggle />
         </div>
     </nav>
   )
